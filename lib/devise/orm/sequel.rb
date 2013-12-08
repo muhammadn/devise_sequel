8916e30e98ab52a12c5f75697e852e83fea0cfe6
Muhammad Nuzaihan Bin Kamal Luddin <muhammad@taqisystems.com>
2013-12-08 11:46:50 +0800

    bug fixes

--- lib/devise/orm/sequel.rb
+++ lib/devise/orm/sequel.rb
@@ -1,35 +1,35 @@
 require 'devise_sequel'
 # require 'devise/schema'
 
-# module Devise
-#  module Orm
-#    module Sequel
-#      module Schema
-#        include Devise::Schema
+ module Devise
+  module Orm
+    module Sequel
+      module Schema
+        include Devise::Schema
 
-#        SCHEMA_OPTIONS = { 
-#          :limit => :size
-#          # there's also precision and scale
-#        }
+        SCHEMA_OPTIONS = { 
+          :limit => :size
+          # there's also precision and scale
+        }
 
-#        def apply_devise_schema(name, type, options={})
-#          SCHEMA_OPTIONS.each do |old_key, new_key|
-#            next unless options.key?(old_key)
-#            options[new_key] = options.delete(old_key)
-#          end
+         def apply_devise_schema(name, type, options={})
+          SCHEMA_OPTIONS.each do |old_key, new_key|
+            next unless options.key?(old_key)
+            options[new_key] = options.delete(old_key)
+          end
 
-#          cname = name.to_s.to_sym
-#          if cname == :email then
-#            # special case for "authenticable" method to also add 
-#            # auto incrementing id, since sequel doesn't do this automatically
-#            primary_key(:id)
-#          end
+          cname = name.to_s.to_sym
+          if cname == :email then
+            # special case for "authenticable" method to also add 
+            # auto incrementing id, since sequel doesn't do this automatically
+            primary_key(:id)
+          end
 
-#          column(cname, type, options)
-#        end
-#      end
-#    end
-#  end
-# end
+          column(cname, type, options)
+        end
+      end
+    end
+  end
+ end
 
-Sequel::Schema::Generator.send(:include, Devise::Orm::Sequel::Schema)
+# Sequel::Schema::Generator.send(:include, Devise::Orm::Sequel::Schema)
